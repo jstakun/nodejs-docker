9cf08e79124c2f77c3240982e83045c8bc5b8abb
Jaroslaw Stakun <jstakun.appspot@gmail.com>
2017-03-30 08:39:22 +0200

    Update server.js

--- server.js
+++ server.js
@@ -8,10 +8,10 @@ const PORT = 8080;
 // App
 const app = express();
 app.get('/', function (req, res) {
-   console.log('Received new request from ' + req.ip);
+   console.log('Received new request from: ' + req.ip);
    res.send('Hello world from WAW\n');
 });
 
 app.listen(PORT);
-console.log('Running on port:' + PORT);
+console.log('Running on port: ' + PORT);
 
